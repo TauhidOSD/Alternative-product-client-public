2e1d9bf4f32080ee9c402559a375f3132b2dc0f9
Tauhid hossen <tauhidhossen04@gmail.com>
2024-05-12 01:25:00 +0600

    server side post data

--- src/Pages/Authentication/AddQuries/AddQuries.jsx
+++ src/Pages/Authentication/AddQuries/AddQuries.jsx
@@ -1,11 +1,136 @@
-
-
 const AddQuries = () => {
-    return (
-        <div>
-            <h1>This add Quries page</h1>
+
+    const handleAddPlace = (event) => {
+        event.preventDefault();
+    
+        const form = event.target;
+        const P_name = form.P_name.value;
+        const P_Brand = form.P_Brand.value;
+        const P_URL = form.P_URL.value;
+        const QueryTitle = form.QueryTitle.value;
+        const BoycottingReason = form.BoycottingReason.value;
+        
+    
+        const Quries = {
+            P_name,
+            P_Brand,
+            P_URL,
+            QueryTitle,
+            BoycottingReason,
+         
+        }
+        console.log(Quries);
+
+        //sent data to the server
+        fetch('http://localhost:5000/newQueries',{
+            method:'POST',
+            headers:{
+                'content-type':'application/json'
+            },
+            body:JSON.stringify(Quries)
+        })
+        .then(res =>res.json())
+        .then(data=>{
+            console.log(data);
+        })
+        
+    }
+  return (
+
+    <div className="bg-[#F4F3F0] p-4 md:p-24">
+      <h2 className="text-3xl font-extrabold">Add Queries</h2>
+      <form onSubmit={handleAddPlace}>
+        {/*image and sportName */}
+        <div className="md:flex mb-6">
+          <div className="form-control md:w-1/2">
+            <label className="label">
+              <span className="label-text">Product Name</span>
+            </label>
+            <label>
+              <input
+                type="text"
+                name="P_name"
+                placeholder="Product Name"
+                className="input input-bordered w-full"
+                required
+              />
+            </label>
+          </div>
+          <div className="form-control md:w-1/2 md:ml-4">
+            <label className="label">
+              <span className="label-text">product Brand</span>
+            </label>
+            <label>
+              <input
+                type="text"
+                name="P_Brand"
+                placeholder="product Brand"
+                className="input input-bordered w-full"
+                required
+              />
+            </label>
+          </div>
         </div>
-    );
+        {/* country and location  */}
+        <div className="md:flex mb-6">
+          <div className="form-control md:w-1/2">
+            <label className="label">
+              <span className="label-text">Product Image-URL</span>
+            </label>
+            <label>
+              <input
+                type="text"
+                name="P_URL"
+                placeholder="Product Image-URL"
+                className="input input-bordered w-full"
+                required
+              />
+            </label>
+          </div>
+          <div className="form-control md:w-1/2 md:ml-4">
+            <label className="label">
+              <span className="label-text">Query Title</span>
+            </label>
+            <label>
+              <input
+                type="text"
+                name="QueryTitle"
+                placeholder="Query Title"
+                className="input input-bordered w-full"
+                required
+              />
+            </label>
+          </div>
+        </div>
+        {/* Description and cost  */}
+        <div className="md:flex mb-6">
+          <div className="form-control md:w-1/2">
+            <label className="label">
+              <span className="label-text">Boycotting Reason</span>
+            </label>
+            <label>
+              <input
+                type="text"
+                name="BoycottingReason"
+                placeholder="Boycotting Reason"
+                className="input input-bordered w-full"
+                required
+              />
+            </label>
+          </div>
+          
+        </div>
+       
+        
+
+        <input
+          type="submit"
+          value="Please Add  "
+          className="btn btn-block bg-slate-700 text-white font-bold text-xl"
+        />
+      </form>
+    </div>
+  );
 };
 
 export default AddQuries;
